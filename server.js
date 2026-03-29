8d39f7d9da6dcf34d4d6660c93db72c40b444efa
don-noel <codeur9.0@gmail.com>
2026-03-29 12:03:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -15,8 +15,6 @@ const app = express();
 // ============================================================
 // VULN-1 (Gitleaks) : Secret JWT hardcodé en clair
 // CORRECTION : remplacer par process.env.JWT_SECRET
-//const JWT_SECRET    = "dvna-pfe-super-secret-jwt-2024";
-//const ADMIN_API_KEY = "sk-dvna-admin-4f8b2c1d9e3a7f6b";
 // ============================================================
 
 const JWT_SECRET    = process.env.JWT_SECRET    || 'changeme-in-prod';
